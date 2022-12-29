767d93e5a4421145443f87c633cbd617fb8576c9
Runner <soloshenko-den@mail.ru>
2022-12-30 00:20:50 +0700

    CreateArray

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+void CreatArr(string[] Ar1)
+{
+    int i = 0;
+    while(i < Ar1.Length)
+    {
+        Console.WriteLine($"Введите {i+1} строку:");
+        Ar1[i] = Console.ReadLine();
+        i++;
+    }
+}
+void PrintArr(string[] Arr)
+{
+    int i = 0;
+    while(i < Arr.Length)
+    {
+        Console.WriteLine($" {Arr[i]} ");
+        i++;
+    }
+}
+Console.WriteLine("Введите длину массива:");
+int Len;
+Len = Convert.ToInt32(Console.ReadLine());
+string[] Arr = new string[Len];
+CreatArr(Arr);
+PrintArr(Arr);
