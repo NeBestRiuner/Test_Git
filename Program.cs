abf6b3872e380d1ad579113814d58eeea7d96f4f
Runner <soloshenko-den@mail.ru>
2022-12-30 01:14:13 +0700

    AlgorithmDelete3

--- Program.cs
+++ Program.cs
@@ -19,9 +19,39 @@ void PrintArr(string[] Arr)
         i++;
     }
 }
+string[] ArrDelete3(string[] Arr)
+{
+    int i = 0;
+    int count = 0;
+    while(i < Arr.Length)
+    {
+        if(Arr[i].Length <= 3)
+        {
+            count++;
+        }
+        i++;
+    }
+    string[] Arr2 = new string[count]; 
+    i=0;
+    count=0;
+    while(i < Arr.Length)
+    {
+        if(Arr[i].Length <= 3) 
+        {
+            Arr2[count]=Arr[i];
+            count++;
+        }
+        i++;
+    }
+    return Arr2;
+}
 Console.WriteLine("Введите длину массива:");
 int Len;
 Len = Convert.ToInt32(Console.ReadLine());
 string[] Arr = new string[Len];
 CreatArr(Arr);
+Console.WriteLine("Введённый массив:");
+PrintArr(Arr);
+Arr = ArrDelete3(Arr);
+Console.WriteLine("Массив только со словами меньше 3:");
 PrintArr(Arr);
